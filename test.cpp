2d1806d6fe42af530420d61aa40deca04791bb1e
Nikolexion <nfalc2003@gmail.com>
2024-04-10 22:40:31 -0400

    Importacion del codigo

---
+++ test.cpp
@@ -0,0 +1,27 @@
+/* Compilación: g++ -g -o img test.cpp
+ * Ejecución: ./img
+ *
+ * Luego de la ejecución se generarán 3 imagines: imagen.png,
+ * imagen_desplazada_500.png e imagen_desplazada_600.png
+ */
+
+#include "moving_image.h"
+#include<unistd.h> // para sleep (linux). Usar  #include<windows.h> para Windows
+
+int main() {
+  moving_image im;
+
+  while(1){
+    im.draw("imagen.png");
+  im.move_right(50);
+  sleep(1);
+  }
+
+  /* NOTA 1: Si usan el mismo nombre para las imágenes, entonces cada llamada al
+  método draw() sobreescribirá a la imagen */
+
+  /* NOTA 2: Si usan un ciclo while(1) y usan el mismo nombre de imagen,
+  entonces se puede generar un efecto de desplazamiento continuo (algo así como
+  un video =D ) */
+  return 0;
+}
